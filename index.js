3e875d2e331351d179c6a332c48905c1b33ad0dc
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2022-12-20 21:53:04 -0700

    commit tree 8451f27f681c35660a2da8aa025b4f36b60cc345

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution in this file!
+let employee = {
+   
+    name: 'John',
+    streetAddress: '121 North Ave',
+
+};
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const updatedEmployee = { ...employee };
+
+    updatedEmployee[key] = value;
+
+    return updatedEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const deleteEmployee = { ...employee };
+
+    delete deleteEmployee[key];
+
+    return deleteEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+
+};
